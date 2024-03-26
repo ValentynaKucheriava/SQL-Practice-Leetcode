72c8723644a58accf5f82018dce1a0e0b591192f
Valentyna Kucheriava <157480181+ValentynaKucheriava@users.noreply.github.com>
2024-03-26 21:22:23 +0100

    Create 610_ Triangle_Judgement.sql

---
+++ Easy/610_ Triangle_Judgement.sql
@@ -0,0 +1,9 @@
+-- https://leetcode.com/problems/triangle-judgement/description/
+
+SELECT MAX(num) AS num
+FROM (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(*) = 1
+) AS unique_numbers;
